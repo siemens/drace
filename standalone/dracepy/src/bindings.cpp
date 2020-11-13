1f5db303aa4d9a98e886bbab1c48fa2c98a77459
Felix Moessbauer <felix.moessbauer@siemens.com>
2020-11-13 11:12:45 +0100

    doc: add python docstrings

--- standalone/dracepy/src/bindings.cpp
+++ standalone/dracepy/src/bindings.cpp
@@ -22,23 +22,39 @@
 BOOST_PYTHON_MODULE(dracepy) {
   // clang-format off
   python::class_<DetectorPy, boost::noncopyable>(
-      "Detector", python::init<const std::string&>())
-      .def("init", &DetectorPy::init)
-      .def("finalize", &DetectorPy::finalize)
-      .def("fork", &DetectorPy::fork)
-      .def("join", &DetectorPy::join)
-      .def("name", &DetectorPy::name)
-      .def("version", &DetectorPy::version);
+      "Detector",
+        "race-detection object that uses the specified backend algorithm",
+        python::init<const std::string&>())
+      .def("init", &DetectorPy::init,
+        "initialize the detector. Has to be called before any analysis function.")
+      .def("finalize", &DetectorPy::finalize,
+        "closes the detection session. A new session can be created with init.")
+      .def("fork", &DetectorPy::fork,
+        "log a thread fork event")
+      .def("join", &DetectorPy::join,
+        "log a thread join event")
+      .def("name", &DetectorPy::name,
+        "return the name of the detector")
+      .def("version", &DetectorPy::version,
+        "return the version of the detector");
 
   python::class_<ThreadStatePy>("ThreadState", python::init<Detector::tls_t, Detector*>())
-      .def("acquire", &ThreadStatePy::acquire)
-      .def("release", &ThreadStatePy::release)
-      .def("happens_before", &ThreadStatePy::happens_before)
-      .def("happens_after", &ThreadStatePy::happens_after)
-      .def("func_enter", &ThreadStatePy::func_enter)
-      .def("func_exit", &ThreadStatePy::func_exit)
-      .def("read", &ThreadStatePy::read)
-      .def("write", &ThreadStatePy::write);
+      .def("acquire", &ThreadStatePy::acquire,
+        "log a mutex aquire event")
+      .def("release", &ThreadStatePy::release,
+        "log a mutex release event")
+      .def("happens_before", &ThreadStatePy::happens_before,
+        "draw a happens before arc")
+      .def("happens_after", &ThreadStatePy::happens_after,
+        "draw a happens after arc")
+      .def("func_enter", &ThreadStatePy::func_enter,
+        "log a function call")
+      .def("func_exit", &ThreadStatePy::func_exit,
+        "log the return from a function")
+      .def("read", &ThreadStatePy::read,
+        "log a memory read")
+      .def("write", &ThreadStatePy::write,
+        "log a memory write");
 
   python::class_<Detector::AccessEntry>("AccessEntry")
       .add_property("write", &Detector::AccessEntry::write)
