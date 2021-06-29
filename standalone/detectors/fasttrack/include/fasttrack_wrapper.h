ba145fe4e1689e2e7740ef7fdf279fa3b81e8252
Mohamad Kanj <mohamad.kanj@siemens.com>
2021-06-29 15:13:06 +0200

    style: add reuse header for fasttrack wrapper

--- standalone/detectors/fasttrack/include/fasttrack_wrapper.h
+++ standalone/detectors/fasttrack/include/fasttrack_wrapper.h
@@ -1,3 +1,17 @@
+#ifndef FASTTRACK_WRAPPER_H
+#define FASTTRACK_WRAPPER_H
+
+/*
+ * DRace, a dynamic data race detector
+ *
+ * Copyright 2020 Siemens AG
+ *
+ * Authors:
+ *   Mihai Robescu <mihai-gabriel.robescu@siemens.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
 #include "fasttrack.h"
 
 namespace drace {
@@ -15,3 +29,5 @@ class FasttrackWrapper : public Fasttrack<LockT> {
 
 }  // namespace detector
 }  // namespace drace
+
+#endif  // !FASTTRACK_WRAPPER_H
